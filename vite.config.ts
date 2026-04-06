d33b3610c7333648a68dfe3b2d25edc40c109727
Prakash Yadav <pk143lovek@gmail.com>
2026-04-06 17:46:53 +0545

    base added

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/", // 🔥 ADD THIS LINE
   plugins: [react()],
   build: {
     rollupOptions: {
@@ -32,4 +32,3 @@ export default defineConfig({
     },
   },
 })
-
